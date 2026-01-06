5d00bf40e30ee4e0557cd0f1c2fbabeaf4fb335b
sudeaydiin <sudea0404@gmail.com>
2026-01-06 15:29:22 +0300

    Refactor TaskListScreen to use LazyColumn
    Updated TaskListScreen to use LazyColumn for task display and added navigation support.

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -1,7 +1,8 @@
 package com.yeditepe.finalexam.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,13 +12,17 @@ import com.yeditepe.finalexam.model.Task
 import com.yeditepe.finalexam.viewmodel.TaskViewModel
 
 @Composable
-fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
+fun TaskListScreen(navController: NavController, viewModel: TaskViewModel = viewModel()) {
 
     // TODO 3: Read task list from ViewModel
+    val tasks = viewModel.tasks
 
-    Column {
-        // TODO 4: Display task titles and completion status
-        // Use a simple Column or LazyColumn
+    // TODO 4: Display task titles and completion status
+    // Use a simple Column or LazyColumn
+    LazyColumn {
+        items(tasks) { task ->
+            TaskRow(task = task, navController = navController)
+        }
     }
 }
 
@@ -25,9 +30,10 @@ fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
 fun TaskRow(task: Task, navController: NavController) {
 
     Text(
-        text = task.title,
+        text = "${task.title} - ${if (task.isCompleted) "Completed" else "Not Completed"}",
         modifier = Modifier.clickable {
             // TODO 3: Navigate to detail screen with task title
+            navController.navigate("taskDetail/${task.title}")
         }
     )
 }
